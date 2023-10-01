b0ee2ce9275fee319d4549d5e3aa1f76b61b4800
Nata Bagus Arisandi <natabagusarisandi68@gmail.com>
2023-10-01 11:59:43 +0700

    fix ui ticket page

--- src/features/admin/tiket/tiket/tiket.tsx
+++ src/features/admin/tiket/tiket/tiket.tsx
@@ -1,14 +1,74 @@
-import { IconButton, Typography } from "@material-tailwind/react";
+import {
+  Avatar,
+  Button,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
 import {
   ArrowLeftIcon,
   ArrowRightIcon,
   ChevronLeft,
   ChevronRight,
   Filter,
+  MoreVertical,
   SortAsc,
 } from "lucide-react";
-
+{
+  /* <td className="text-sm py-2 border-b border-gray-400 text-center flex px-4 w-fit">
+  <div className="flex gap-2 ">
+    <Avatar
+      src="https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHdvbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+      alt="avatar"
+    />
+    <div className="flex flex-col items-start">
+      <h1 className="font-[600] text-[12px]">Contact Email not Linked</h1>
+      <p className="font-[400] text-[10px] text-gray-400">Updated 1 day ago</p>
+    </div>
+  </div>
+</td>; */
+}
 export const Tiket = () => {
+  const TABLE_HEAD = [
+    "Ticket details",
+    "Customer name",
+    "Date",
+    "Priority",
+    "",
+  ];
+
+  const TABLE_ROWS = [
+    {
+      name: "John Michael",
+      job: "Manager",
+      date: "23/04/18",
+      priority: "boom",
+    },
+    {
+      name: "Alexa Liras",
+      job: "Developer",
+      date: "23/04/18",
+      priority: "boom",
+    },
+    {
+      name: "Laurent Perrier",
+      job: "Executive",
+      date: "19/09/17",
+      priority: "boom",
+    },
+    {
+      name: "Michael Levi",
+      job: "Developer",
+      date: "24/12/08",
+      priority: "boom",
+    },
+    {
+      name: "Richard Gran",
+      job: "Manager",
+      date: "04/10/21",
+      priority: "boom",
+    },
+  ];
+
   return (
     <main>
       <div className="w-full border border-gray-200 rounded-lg bg-white">
@@ -26,41 +86,96 @@ export const Tiket = () => {
           </div>
         </header>
         {/* table */}
+
         <div className="w-full">
-          <table className="w-full">
+          <table className="w-full min-w-max table-auto text-left">
             <thead>
               <tr>
-                <th className="text-sm text-gray-400 py-2 border-b-2 border-gray-400">
-                  Ticket detail
-                </th>
-                <th className="text-sm text-gray-400 py-2 border-b-2 border-gray-400">
-                  Customer name
-                </th>
-                <th className="text-sm text-gray-400 py-2 border-b-2 border-gray-400">
-                  Date
-                </th>
-                <th className="text-sm text-gray-400 py-2 border-b-2 border-gray-400">
-                  Priority
-                </th>
-                <th className="text-sm text-gray-400 py-2 border-b-2 border-gray-400"></th>
+                {TABLE_HEAD.map((head) => (
+                  <th key={head} className="border-b border-blue-gray-100 p-4">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="text-sm py-2 border-b border-gray-400 text-center">
-                  Ticket detail
-                </td>
-                <td className="text-sm py-2 border-b border-gray-400 text-center">
-                  Customer name
-                </td>
-                <td className="text-sm py-2 border-b border-gray-400 text-center">
-                  Date
-                </td>
-                <td className="text-sm py-2 border-b border-gray-400 text-center">
-                  Priority
-                </td>
-                <td className="text-sm py-2 border-b border-gray-400 text-center"></td>
-              </tr>
+              {TABLE_ROWS.map(({ name, job, date, priority }, index) => {
+                const isLast = index === TABLE_ROWS.length - 1;
+                const classes = isLast
+                  ? "p-4"
+                  : "p-4 border-b border-blue-gray-50";
+
+                return (
+                  <tr key={name}>
+                    <td className={classes}>
+                      <div className="flex gap-2 ">
+                        <Avatar
+                          src="https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHdvbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                          alt="avatar"
+                        />
+                        <div className="flex flex-col gap-2 items-start">
+                          <h1 className="font-[600] text-[12px]">
+                            Contact Email not Linked
+                          </h1>
+                          <p className="font-[400] text-[10px] text-gray-400">
+                            Updated 1 day ago
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="flex flex-col gap-2 w-fit items-center">
+                        <Typography
+                          variant="h6"
+                          className="font-bold text-gray-600"
+                        >
+                          {name}
+                        </Typography>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-gray-600 text-[10px]"
+                        >
+                          {date}
+                        </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <div className="flex flex-col gap-2 w-fit items-start">
+                        <Typography
+                          variant="h6"
+                          className="font-bold text-gray-600"
+                        >
+                          {date}
+                        </Typography>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal text-gray-600 text-[10px]"
+                        >
+                          6:30 PM
+                        </Typography>
+                      </div>
+                    </td>
+                    <td className={classes}>
+                      <Button color="red" className="p-2 rounded-full">
+                        High
+                      </Button>
+                    </td>
+                    <td className={classes}>
+                      <button>
+                        <MoreVertical className="w-5 h-5 text-gray-400" />
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
